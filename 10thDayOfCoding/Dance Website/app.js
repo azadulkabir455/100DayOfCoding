1adb0c07209a9842b48ebb951993807dc4c6e5f2
Azad Ul Kabir <azadulkabir10@gmail.com>
2020-10-19 21:39:49 +0600

    Create a dancing website

---
+++ 10thDayOfCoding/Dance Website/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use("/public",express.static("public"));
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req,res) => {
+    res.status(200).render("index.pug");
+})
+app.get('*',(req,res) => {
+    res.status(404).send("404 not found");
+})
+
+
+app.listen(port, () => {
+    console.log(`App is successfully starting at localhost:${port}`)
+})
